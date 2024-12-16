2265fee91db0156d91f0bfb233b3e77482dbc2e5
Cole Resurreccion <colerezi30@gmail.com>
2024-12-16 12:01:50 -0500

    fixed submod

---
+++ reinforcement_learning/carla_env_old/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='carla_env',
+    version='0.1.0',
+    packages=find_packages(),  # Automatically find packages
+    url='',
+    license='',
+    author='Jan Bieser',
+    author_email='uoqox@student.kit.edu',
+    description=''
+)
